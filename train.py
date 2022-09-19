048c2b817938cd399cdbe9755523ca4a490537f4
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-09-19 15:56:09 +0700

    added training file

--- train.py
+++ train.py
@@ -1,1 +1,14 @@
+import argparse
+
+from configs.utils import get_config
 from builders.task_builder import build_task
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config-file", type=str, required=True)
+
+args = parser.parse_args()
+
+config = get_config(args.config_file)
+
+task = build_task(config)
+task.start()

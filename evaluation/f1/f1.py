cb146a6a700264a7828e6951bb5938dfca24bd1c
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-09-24 18:46:42 +0700

    updated accuracy and f1 scores from dev branch

--- evaluation/f1/f1.py
+++ evaluation/f1/f1.py
@@ -12,21 +12,24 @@ class F1:
         scores = []
         for key in res:
             r = res[key]
-            gt = gts[key]
-            
-            # if either the prediction or the truth is no-answer then f1 = 1 if they agree, 0 otherwise
-            if len(r) == 0 or len(gt) == 0:
-                scores.append(int(r == gt))
-            else:
-                common_tokens = set(r) & set(gt)
-                # if there are no common tokens then f1 = 0
-                if len(common_tokens) == 0:
-                    scores.append(0)
+            scores_per_res = []
+            for gt in gts[key]:
+                # if either the prediction or the truth is no-answer then f1 = 1 if they agree, 0 otherwise
+                if len(r) == 0 or len(gt) == 0:
+                    scores_per_res.append(int(r == gt))
                 else:
-                    prec = len(common_tokens) / len(r)
-                    rec = len(common_tokens) / len(gt)
+                    common_tokens = set(r) & set(gt)
+                    # if there are no common tokens then f1 = 0
+                    if len(common_tokens) == 0:
+                        scores_per_res.append(0)
+                    else:
+                        prec = len(common_tokens) / len(r)
+                        rec = len(common_tokens) / len(gt)
 
-                    scores.append(2*(prec*rec)/(prec+rec))
+                        scores_per_res.append(2*(prec*rec)/(prec+rec))
+
+            scores_per_res = np.array(scores_per_res).mean()
+            scores.append(scores_per_res)
 
         scores = np.array(scores)
 

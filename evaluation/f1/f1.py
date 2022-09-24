859bc1e9dbff92b0681a875c61f81c2c91c8524b
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-09-24 14:32:49 +0700

    updated accuracy score and f1 score from dev branch

--- evaluation/f1/f1.py
+++ evaluation/f1/f1.py
@@ -9,10 +9,16 @@ class F1_micro:
                 res (dict)  : dictionary with key <image> and value <tokenized reference sentence>
         :return: f1 (float) : computed F1 score for the corpus
         """
-        res = {key: value.split() for key, value in res.items()}
+        res = {key: value[0].split() for key, value in res.items()}
         scores = []
         for key in res:
-            scores.append(f1_score(gts[key], res[key], average="micro", zero_division=0))
+            r = res[key]
+            g = gts[key]
+            if len(r) > len(g):
+                r = r[:len(g)]
+            else:
+                r = r + ["<pad>"]*(len(g) - len(r))
+            scores.append(f1_score(g, r, average="micro", zero_division=0))
 
         scores = np.array(scores)
 
@@ -29,10 +35,16 @@ class F1_macro:
                 res (dict)  : dictionary with key <image> and value <tokenized reference sentence>
         :return: f1 (float) : computed F1 score for the corpus
         """
-        res = {key: value.split() for key, value in res.items()}
+        res = {key: value[0].split() for key, value in res.items()}
         scores = []
         for key in res:
-            scores.append(f1_score(gts[key], res[key], average="macro", zero_division=0))
+            r = res[key]
+            g = gts[key]
+            if len(r) > len(g):
+                r = r[:len(g)]
+            else:
+                r = r + ["<pad>"]*(len(g) - len(r))
+            scores.append(f1_score(g, r, average="macro", zero_division=0))
 
         scores = np.array(scores)
 

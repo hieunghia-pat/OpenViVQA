6dc43ac3583e81ce31baa47e60ea53a178fd598a
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-09-24 11:17:54 +0700

    updated open-ended training task from master branch

--- tasks/open_ended_task.py
+++ tasks/open_ended_task.py
@@ -83,7 +83,7 @@ class OpenEndedTask(BaseTask):
             for it, items in enumerate(dataloader):
                 items = items.to(self.device)
                 with torch.no_grad():
-                    outs, _ = self.model.beam_search(items, batch_size=dataloader.batch_size, beam_size=self.evaluating_beam_size, out_size=1)
+                    outs, _ = self.model.beam_search(items, batch_size=items.batch_size, beam_size=self.evaluating_beam_size, out_size=1)
 
                 answers_gt = items.answer
                 answers_gen = self.vocab.decode_answer(outs.contiguous().view(-1, self.vocab.max_answer_length), join_words=False)
@@ -229,7 +229,7 @@ class OpenEndedTask(BaseTask):
 
             self.epoch += 1
 
-    def get_predictions(self, get_scores=True):
+    def get_predictions(self, dataset, get_scores=True):
         if not os.path.isfile(os.path.join(self.checkpoint_path, 'best_model.pth')):
             logger.error("Prediction require the model must be trained. There is no weights to load for model prediction!")
             raise FileNotFoundError("Make sure your checkpoint path is correct or the best_model.pth is available in your checkpoint path")
@@ -239,9 +239,8 @@ class OpenEndedTask(BaseTask):
 
         self.model.eval()
         results = []
-        with tqdm(desc='Getting predictions: ', unit='it', total=len(self.test_dataset)) as pbar:
-            for it, items in enumerate(self.test_dataset):
-                items = items.unsqueeze(0)
+        with tqdm(desc='Getting predictions: ', unit='it', total=len(dataset)) as pbar:
+            for it, items in enumerate(dataset):
                 items = items.to(self.device)
                 with torch.no_grad():
                     outs, _ = self.model.beam_search(items, beam_size=self.evaluating_beam_size, out_size=1)

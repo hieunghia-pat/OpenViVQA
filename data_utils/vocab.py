7e6505c48c08905e52fa673437d45a6fc4a61f64
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-10-06 16:13:43 +0700

    updated vocab classes

--- data_utils/vocab.py
+++ data_utils/vocab.py
@@ -8,7 +8,7 @@ from transformers import AutoTokenizer
 
 from collections import defaultdict, Counter
 import json
-from typing import List
+from typing import Dict, List
 
 @META_VOCAB.register()
 class Vocab(object):
@@ -397,6 +397,117 @@ class MultiModalVocab(Vocab):
         self.img_token = config.VOCAB.IMG_TOKEN
         self.feat_token = config.VOCAB.FEAT_TOKEN
         self.box_token = config.VOCAB.BOX_TOKEN
+        self.question_token = config.VOCAB.QUESTION_TOKEN
+        self.answer_token = config.VOCAB.ANSWER_TOKEN
+
+        self.make_vocab([
+            config.JSON_PATH.TRAIN,
+            config.JSON_PATH.DEV,
+            config.JSON_PATH.TEST
+        ])
+        counter = self.freqs.copy()
+    
+        min_freq = max(config.MIN_FREQ, 1)
+
+        specials = [self.padding_token, self.bos_token, self.eos_token, self.unk_token, self.img_token,
+                    self.feat_token, self.box_token, self.question_token, self.answer_token]
+        self.itos = specials
+        # frequencies of special tokens are not counted when building vocabulary
+        # in frequency order
+        for tok in specials:
+            del counter[tok]
+
+        # sort by frequency, then alphabetically
+        words_and_frequencies = sorted(counter.items(), key=lambda tup: tup[0])
+        words_and_frequencies.sort(key=lambda tup: tup[1], reverse=True)
+
+        for word, freq in words_and_frequencies:
+            if freq < min_freq:
+                break
+            self.itos.append(word)
+
+        self.stoi = defaultdict()
+        # stoi is simply a reverse dict for itos
+        self.stoi.update({tok: i for i, tok in enumerate(self.itos)})
+
+        self.padding_idx = self.stoi[self.padding_token]
+        self.bos_idx = self.stoi[self.bos_token]
+        self.eos_idx = self.stoi[self.eos_token]
+        self.unk_idx = self.stoi[self.unk_token]
+        self.img_idx = self.stoi[self.img_token]
+        self.feat_idx = self.stoi[self.feat_token]
+        self.box_idx = self.stoi[self.box_token]
+        self.question_idx = self.stoi[self.question_token]
+        self.answer_idx = self.stoi[self.answer_token]
+
+        self.specials = specials
+
+        self.word_embeddings = None
+        if config.VOCAB.WORD_EMBEDDING is not None:
+            self.load_word_embeddings(build_word_embedding(config))
+
+@META_VOCAB.register()
+class MultilingualMultiModalVocab(MultiModalVocab):
+    def __init__(self, config) -> None:
+        super().__init__(config)
+
+    def make_vocab(self, json_dirs):
+        self.freqs = Counter()
+        self.max_question_length = 0
+        self.max_answer_length = 0
+        for json_dir in json_dirs:
+            json_data = json.load(open(json_dir))
+            for ann in json_data["annotations"]:
+                for answer in ann["answers"]:
+                    question = ann["question"]
+                    if is_japanese_sentence(question):
+                        question = list(question)
+                        answer = list(answer)
+                    else: # This is Vietnamese or English annotation
+                        question = preprocess_sentence(ann["question"], self.tokenizer)
+                        answer = preprocess_sentence(answer, self.tokenizer)
+                    self.freqs.update(question)
+                    self.freqs.update(answer)
+                    if len(question) + 2 > self.max_question_length:
+                            self.max_question_length = len(question) + 2
+                    if len(answer) + 2 > self.max_answer_length:
+                        self.max_answer_length = len(answer) + 2
+
+@META_VOCAB.register()
+class OcrVocab(MultiModalVocab):
+    '''
+        This class is designed especially for VQA with reading comprehension
+    '''
+    def __init__(self, config):
+
+        self.tokenizer = config.VOCAB.TOKENIZER
+
+        if config.VOCAB.PRETRAINED_LANGUAGE_MODEL is not None: # use special tokens and vocab from pretrained language model
+            token_encoder = AutoTokenizer.from_pretrained(config.VOCAB.PRETRAINED_LANGUAGE_MODEL)
+            token_encoder = AutoTokenizer.from_pretrained(config.VOCAB.PRETRAINED_LANGUAGE_MODEL)
+            self.padding_token = token_encoder.pad_token
+            self.unk_token = token_encoder.unk_token
+            cls_token = token_encoder.cls_token
+            sep_token = token_encoder.sep_token
+            if cls_token is None:
+                cls_token = token_encoder.pad_token
+            if sep_token is None:
+                sep_token = token_encoder.pad_token
+            self.bos_token = token_encoder.bos_token
+            if self.bos_token is None:
+                self.bos_token = cls_token
+            self.eos_token = token_encoder.eos_token
+            if self.eos_token is None:
+                self.eos_token = sep_token
+        else: # use defined special tokens
+            self.padding_token = config.VOCAB.PAD_TOKEN
+            self.bos_token = config.VOCAB.BOS_TOKEN
+            self.eos_token = config.VOCAB.EOS_TOKEN
+            self.unk_token = config.VOCAB.UNK_TOKEN
+
+        self.img_token = config.VOCAB.IMG_TOKEN
+        self.feat_token = config.VOCAB.FEAT_TOKEN
+        self.box_token = config.VOCAB.BOX_TOKEN
         self.ocr_token = config.VOCAB.OCR_TOKEN
         self.ocr_det_token = config.VOCAB.OCR_DET_TOKEN
         self.ocr_rec_token = config.VOCAB.OCR_REC_TOKEN
@@ -453,32 +564,26 @@ class MultiModalVocab(Vocab):
         if config.VOCAB.WORD_EMBEDDING is not None:
             self.load_word_embeddings(build_word_embedding(config))
 
-@META_VOCAB.register()
-class MultilingualMultiModalVocab(MultiModalVocab):
-    def __init__(self, config) -> None:
-        super().__init__(config)
+    def encode_answer(self, answer: str, ocr_ids_of: Dict[str, int]) -> torch.Tensor:
+        """ Turn a answer into a vector of indices and a question length """
+        vec = torch.ones(self.max_answer_length).long() * self.padding_idx
+        for i, token in enumerate([self.bos_token] + answer + [self.eos_token]):
+            vec[i] = self.stoi[token] if token in self.stoi else ocr_ids_of[token]
+        return vec
 
-    def make_vocab(self, json_dirs):
-        self.freqs = Counter()
-        self.max_question_length = 0
-        self.max_answer_length = 0
-        for json_dir in json_dirs:
-            json_data = json.load(open(json_dir))
-            for ann in json_data["annotations"]:
-                for answer in ann["answers"]:
-                    question = ann["question"]
-                    if is_japanese_sentence(question):
-                        question = list(question)
-                        answer = list(answer)
-                    else: # This is Vietnamese or English annotation
-                        question = preprocess_sentence(ann["question"], self.tokenizer)
-                        answer = preprocess_sentence(answer, self.tokenizer)
-                    self.freqs.update(question)
-                    self.freqs.update(answer)
-                    if len(question) + 2 > self.max_question_length:
-                            self.max_question_length = len(question) + 2
-                    if len(answer) + 2 > self.max_answer_length:
-                        self.max_answer_length = len(answer) + 2
+    def decode_answer(self, answer_vecs: torch.Tensor, ocr_token_of: Dict[int, str], join_words=True) -> List[str]:
+        '''
+            answer_vecs: (bs, max_length)
+        '''
+        answers = []
+        for vec in answer_vecs:
+            answer = " ".join([self.itos[idx] if self.itos[idx] not in self.specials and idx in self.itos else ocr_token_of[idx] for idx in vec.tolist()])
+            if join_words:
+                answers.append(answer)
+            else:
+                answers.append(answer.strip().split())
+
+        return answers
 
 @META_VOCAB.register()
 class VlspVqaMultiModalVocab(MultilingualMultiModalVocab):

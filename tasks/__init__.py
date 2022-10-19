7771e2ae19b4e8b9d748b05ddde7e32e1e99acce
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-10-19 17:58:52 +0700

    reimplement the M4C following the original version, rename the previous M4C method to Iterative M4C

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,4 +2,5 @@ from .classification_task import ClassificationTask
 from .open_ended_task import OpenEndedTask
 from .ocr_open_ended_task import OcrOpenEndedTask
 from .training_saaa_task import TrainingSAAATask
+from .training_m4c_task import TrainingM4C
 from .vlsp_evjvqa_task import VlspEvjVqaTask

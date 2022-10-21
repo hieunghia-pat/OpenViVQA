106ee4cf1ec6812019566e3f3b1317ab67f61eec
TOMphong <93088029+TOMphong@users.noreply.github.com>
2022-10-21 18:33:27 +0700

    commit to Phong-dev

---
+++ tasks/assemble_base_task.py
@@ -0,0 +1,106 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.nn import NLLLoss
+from torch.optim import Adam
+from torch.optim.lr_scheduler import LambdaLR
+from builders.vocab_builder import build_vocab
+
+from utils.logging_utils import setup_logger
+from builders.model_builder import build_model
+
+import os
+import numpy as np
+import pickle
+import random
+
+logger = setup_logger()
+
+class AssembleBaseTask:
+    def __init__(self, config):
+
+        self.checkpoint_path = [os.path.join(config.PRETRAINED_MODELS.E_MODEL_PATH, config.MODEL.NAME),
+                                os.path.join(config.PRETRAINED_MODELS.V_MODEL_PATH, config.MODEL.NAME),
+                                os.path.join(config.PRETRAINED_MODELS.J_MODEL_PATH, config.MODEL.NAME)]
+        self.vocab = []
+        for ckpt_path in self.checkpoint_path:
+            if not os.path.isdir(ckpt_path):
+                raise Exception(f"{ckpt_path} does not exist")
+            if not os.path.isfile(os.path.join(ckpt_path, "vocab.bin")):
+                raise Exception(f"vocab file does not exist")
+            else:
+                logger.info("Loading vocab from %s" % os.path.join(ckpt_path, "vocab.bin"))
+                self.vocab.append(pickle.load(open(os.path.join(ckpt_path, "vocab.bin"), "rb")))
+
+        logger.info("Loading data")
+        self.load_datasets(config.DATASET)
+        self.create_dataloaders(config)
+
+        logger.info("Building model")
+        self.model = [build_model(config.MODEL, self.vocab[0]), 
+                    build_model(config.MODEL, self.vocab[1]),
+                    build_model(config.MODEL, self.vocab[2])]
+        self.config = config
+        self.device = torch.device(config.MODEL.DEVICE)
+
+        logger.info("Defining optimizer and objective function")
+        self.configuring_hyperparameters(config)
+        # self.optim = [Adam(self.model[0].parameters(), lr=config.TRAINING.LEARNING_RATE, betas=(0.9, 0.98)),
+        #             Adam(self.model[1].parameters(), lr=config.TRAINING.LEARNING_RATE, betas=(0.9, 0.98)),
+        #             Adam(self.model[2].parameters(), lr=config.TRAINING.LEARNING_RATE, betas=(0.9, 0.98))]
+        
+        
+        # self.scheduler = [LambdaLR(self.optim[0], self.e_lambda_lr),
+        #                 LambdaLR(self.optim[1], self.v_lambda_lr),
+        #                 LambdaLR(self.optim[2], self.j_lambda_lr)]
+
+        # self.loss_fn = NLLLoss(ignore_index=self.vocab.padding_idx)
+
+    def configuring_hyperparameters(self, config):
+        raise NotImplementedError
+
+    def load_vocab(self, config):
+        vocab = build_vocab(config)
+
+        return vocab
+    
+    def load_datasets(self, config):
+        raise NotImplementedError
+
+    def create_dataloaders(self, config):
+        raise NotImplementedError
+
+    def evaluate_loss(self, dataloader: DataLoader):
+        raise NotImplementedError
+
+    def evaluate_metrics(self, dataloader: DataLoader):
+        raise NotImplementedError
+
+    def train(self):
+        raise NotImplementedError
+
+    
+    def load_checkpoint(self, fname, index) -> dict:
+        if not os.path.exists(fname):
+            return None
+
+        logger.info("Loading checkpoint from %s", fname)
+
+        checkpoint = torch.load(fname)
+
+        torch.set_rng_state(checkpoint['torch_rng_state'])
+        torch.cuda.set_rng_state(checkpoint['cuda_rng_state'])
+        np.random.set_state(checkpoint['numpy_rng_state'])
+        random.setstate(checkpoint['random_rng_state'])
+
+        self.model[index].load_state_dict(checkpoint['state_dict'], strict=False)
+
+        logger.info("Resuming from epoch %s", checkpoint['epoch'])
+
+        return checkpoint
+
+    
+    def start(self):
+        raise NotImplementedError
+
+    def get_predictions(self, dataset, get_scores=True):
+        raise NotImplementedError
---
+++ tasks/assemble_mcan_vlsp.py
@@ -0,0 +1,129 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+
+from utils.logging_utils import setup_logger
+from data_utils.utils import collate_fn
+from .assemble_base_task import AssembleBaseTask
+from builders.task_builder import META_TASK
+from builders.dataset_builder import build_dataset
+import evaluation
+from evaluation import Cider
+
+import os
+import numpy as np
+from tqdm import tqdm
+import itertools
+from shutil import copyfile
+import json
+
+logger = setup_logger()
+
+@META_TASK.register()
+class AssembleTask(AssembleBaseTask):
+    '''
+        This task is designed especially for EVJVQA task at VLSP2022
+    '''
+    def __init__(self, config):
+        super().__init__(config)
+        self.results_path = config.PRETRAINED_MODELS.RESULTS_PATH
+    def load_feature_datasets(self, config):        
+        e_dataset = build_dataset(config.JSON_PATH.E_DATASET, self.vocab[0], config.FEATURE_DATASET)
+        v_dataset = build_dataset(config.JSON_PATH.V_DATASET, self.vocab[1], config.FEATURE_DATASET)
+        j_dataset = build_dataset(config.JSON_PATH.J_DATASET, self.vocab[2], config.FEATURE_DATASET)
+        
+        return [e_dataset, v_dataset, j_dataset]
+
+    def load_datasets(self, config):
+        self.dataset = self.load_feature_datasets(config)
+        self.train_dict_dataset, self.dev_dict_dataset, self.public_test_dict_dataset, self.private_test_dict_dataset = self.load_dict_datasets(config)
+
+    def create_feature_dataloaders(self, config):
+        # creating iterable-dataset data loader
+        self.e_dataloader = DataLoader(
+            dataset=self.dataset[0],
+            batch_size=config.DATASET.FEATURE_DATASET.BATCH_SIZE,
+            shuffle=True,
+            num_workers=config.DATASET.FEATURE_DATASET.WORKERS,
+            collate_fn=collate_fn
+        )
+        self.v_dataloader = DataLoader(
+            dataset=self.dataset[1],
+            batch_size=config.DATASET.FEATURE_DATASET.BATCH_SIZE,
+            shuffle=True,
+            num_workers=config.DATASET.FEATURE_DATASET.WORKERS,
+            collate_fn=collate_fn
+        )
+        self.j_dataloader = DataLoader(
+            dataset=self.dataset[0],
+            batch_size=config.DATASET.FEATURE_DATASET.BATCH_SIZE,
+            shuffle=True,
+            num_workers=config.DATASET.FEATURE_DATASET.WORKERS,
+            collate_fn=collate_fn
+        ) 
+
+    def configuring_hyperparameters(self, config):
+        self.epoch = 0
+        self.warmup = config.TRAINING.WARMUP
+        self.score = config.TRAINING.SCORE
+        self.learning_rate = config.TRAINING.LEARNING_RATE
+        self.rl_learning_rate = config.TRAINING.RL_LEARNING_RATE
+        self.training_beam_size = config.TRAINING.TRAINING_BEAM_SIZE
+        self.evaluating_beam_size = config.TRAINING.EVALUATING_BEAM_SIZE
+        self.patience = config.TRAINING.PATIENCE
+        self.train_cider = Cider({f"{idx}": answer for idx, answer in enumerate(self.train_dataset.answers)})
+
+    def start(self):
+        pass
+
+    def get_predictions(self):
+        for idx, ckpt_path in enumerate(self.checkpoint_path):
+            if not os.path.isfile(os.path.join(ckpt_path, 'best_model.pth')):
+                logger.error("Prediction require the model must be trained. There is no weights to load for model prediction!")
+                raise FileNotFoundError("Make sure your checkpoint path is correct or the best_model.pth is available in your checkpoint path")
+
+            self.load_checkpoint(os.path.join(ckpt_path, "best_model.pth"), idx)
+            self.model[idx].eval()
+        
+        results = []
+        overall_gens = {}
+        overall_gts = {}
+        for idx, data in enumerate(self.dataset):
+            if data is not None:
+                with tqdm(desc='Getting predictions on public test: ', unit='it', total=len(data)) as pbar:
+                    for it, items in enumerate(data):
+                        items = items.to(self.device)
+                        with torch.no_grad():
+                            outs, _ = self.model[idx].beam_search(items, batch_size=items.batch_size, beam_size=self.evaluating_beam_size, out_size=1)
+
+                        answers_gt = items.answers
+                        answers_gen = self.vocab[idx].decode_answer(outs.contiguous().view(-1, self.vocab[idx].max_answer_length), join_words=False)
+                        gts = {}
+                        gens = {}
+                        for i, (gts_i, gen_i) in enumerate(zip(answers_gt, answers_gen)):
+                            gen_i = ' '.join([k for k, g in itertools.groupby(gen_i)])
+                            gens['%d_%d' % (it, i)] = gen_i
+                            gts['%d_%d' % (it, i)] = gts_i
+                            overall_gens['%d_%d' % (it, i)] = [gen_i, ]
+                            overall_gts['%d_%d' % (it, i)] = gts_i
+                        pbar.update()
+
+                        results.append({
+                            "id": items.question_id,
+                            "image_id": items.image_id,
+                            "filename": items.filename,
+                            "gens": gens,
+                            "gts": gts
+                        })
+
+                        pbar.update()
+
+        scores, _ = evaluation.compute_scores(overall_gts, overall_gens)
+        logger.info("Evaluation score on public test: %s", scores)
+
+        json.dump({
+            "results": results,
+            **scores
+        }, open(os.path.join(self.results_path, "public_test_results.json"), "w+"), ensure_ascii=False)
+
+        

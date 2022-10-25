3d9b18b352794d96801b63d5f829364b83061b14
quan nguyen <you@example.com>
2022-10-26 00:47:24 +0700

    update config cross mbert, rawquestion dataset, enjavi vocab

--- data_utils/dataset.py
+++ data_utils/dataset.py
@@ -742,3 +742,109 @@ class MultilingualImageQuestionClassificationDataset(ImageQuestionClassification
             annotations.append(annotation)
 
         return annotations    
+
+
+@META_DATASET.register()
+class RawQuestionMultilingualFeatureDataset(BaseDataset):
+    def __init__(self, json_path: str, vocab, config) -> None:
+        super().__init__(json_path, vocab, config)
+
+    @property
+    def questions(self):
+        return [ann["question"] for ann in self.annotations]
+
+    @property
+    def answers(self):
+        return [ann["answer"] for ann in self.annotations]
+
+    def load_annotations(self, json_data: Dict) -> List[Dict]:
+        annotations = []
+        for ann in json_data["annotations"]:
+            # find the appropriate image
+            for image in json_data["images"]:
+                if image["id"] == ann["image_id"]:
+                    answer = ann["answer"]
+                    question = ann["question"]
+                    if is_japanese_sentence(question):
+                        answer = list(answer)
+                    else:
+                        answer = preprocess_sentence(answer, self.vocab.tokenizer)
+                    annotation = {
+                        "question": question,
+                        "answer": answer,
+                        "image_id": ann["image_id"],
+                        "filename": image["filename"]
+                    }
+                    annotations.append(annotation)
+                    break
+        return annotations
+
+    def __getitem__(self, idx: int):
+        item = self.annotations[idx]
+        question = item["question"]
+        answer = self.vocab.encode_answer(item["answer"])
+
+        shifted_right_answer = torch.zeros_like(answer).fill_(self.vocab.padding_idx)
+        shifted_right_answer[:-1] = answer[1:]
+        answer = torch.where(answer == self.vocab.eos_idx, self.vocab.padding_idx, answer) # remove eos_token in answer
+        
+        features = self.load_features(self.annotations[idx]["image_id"])
+
+        return Instance(
+            question=question,
+            answer_tokens=answer,
+            shifted_right_answer_tokens=shifted_right_answer,
+            **features,
+        )
+
+    def __len__(self) -> int:
+        return len(self.annotations)
+
+@META_DATASET.register()
+class RawQuestionMultilingualDictionaryDataset(BaseDataset):
+    def __init__(self, json_path: str, vocab, config) -> None:
+        super(RawQuestionMultilingualDictionaryDataset,self).__init__(json_path, vocab, config)
+
+    def load_annotations(self, json_data: Dict) -> List[Dict]:
+        annotations = []
+        for ann in json_data["annotations"]:
+            # find the appropriate image
+            for image in json_data["images"]:
+                if image["id"] == ann["image_id"]:
+                    question = ann["question"]
+                    answer = ann["answer"]
+                    if is_japanese_sentence(question):
+                        answer = list(answer)
+                    else:
+                        answer = preprocess_sentence(answer, self.vocab.tokenizer)
+                    annotation = {
+                        "question_id": ann["id"],
+                        # "type": ann["QA-type"],
+                        "question": question,
+                        "answer": answer,
+                        "image_id": ann["image_id"],
+                        "filename": image["filename"]
+                    }
+                    break
+
+            annotations.append(annotation)
+
+        return annotations
+
+    def __getitem__(self, idx: int):
+        item = self.annotations[idx]
+        image_id = item["image_id"]
+        filename = item["filename"]
+        features = self.load_features(image_id)
+        question = item["question"]
+        answer = item["answer"]
+
+        return Instances(
+            question_id=item["question_id"],
+            # type=item["type"],
+            image_id=image_id,
+            filename=filename,
+            question=question,
+            answer=answer,
+            **features
+        )
--- data_utils/vocab.py
+++ data_utils/vocab.py
@@ -586,3 +586,56 @@ class VlspVqaMultiModalVocab(MultilingualMultiModalVocab):
         self.word_embeddings = None
         if config.VOCAB.WORD_EMBEDDING is not None:
             self.load_word_embeddings(build_word_embedding(config))
+
+
+@META_VOCAB.register()
+class VlspEvjVqaVocab(MultilingualVocab):
+    '''
+        This vocab is designed specially for EVJVQA dataset
+    '''
+    
+    def __init__(self, config) -> None:
+        self.tokenizer = config.VOCAB.TOKENIZER
+
+        self.padding_token = config.VOCAB.PAD_TOKEN
+        self.bos_token = config.VOCAB.BOS_TOKEN
+        self.eos_token = config.VOCAB.EOS_TOKEN
+        self.unk_token = config.VOCAB.UNK_TOKEN
+
+        self.make_vocab([
+            config.JSON_PATH.TRAIN,
+            config.JSON_PATH.DEV
+        ])
+        counter = self.freqs.copy()
+    
+        min_freq = max(config.MIN_FREQ, 1)
+
+        specials = [self.padding_token, self.bos_token, self.eos_token, self.unk_token]
+        itos = specials
+        # frequencies of special tokens are not counted when building vocabulary
+        # in frequency order
+        for tok in specials:
+            del counter[tok]
+
+        # sort by frequency, then alphabetically
+        words_and_frequencies = sorted(counter.items(), key=lambda tup: tup[0])
+        words_and_frequencies.sort(key=lambda tup: tup[1], reverse=True)
+
+        for word, freq in words_and_frequencies:
+            if freq < min_freq:
+                break
+            itos.append(word)
+
+        self.itos = {i: tok for i, tok in enumerate(itos)}
+        self.stoi = {tok: i for i, tok in enumerate(itos)}
+
+        self.specials = [self.padding_token, self.bos_token, self.eos_token, self.unk_token]
+
+        self.padding_idx = self.stoi[self.padding_token]
+        self.bos_idx = self.stoi[self.bos_token]
+        self.eos_idx = self.stoi[self.eos_token]
+        self.unk_idx = self.stoi[self.unk_token]
+
+        self.word_embeddings = None
+        if config.VOCAB.WORD_EMBEDDING is not None:
+            self.load_word_embeddings(build_word_embedding(config))

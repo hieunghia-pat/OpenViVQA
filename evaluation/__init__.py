a62e9a0b8faa8b1030e537402ecff73f75da1e5b
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-11-14 11:19:35 +0700

    added accuracy from dev branch

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -2,12 +2,13 @@ from .bleu import Bleu
 from .meteor import Meteor
 from .rouge import Rouge
 from .cider import Cider
+from .accuracy import Accuracy
 from .f1 import F1
 from .precision import Precision
 from .recall import Recall
 
 def compute_scores(gts, gen):
-    metrics = (Bleu(), Meteor(), Rouge(), Cider(), Precision(), Recall(), F1())
+    metrics = (Bleu(), Meteor(), Rouge(), Cider(), Accuracy(), Precision(), Recall(), F1())
     all_score = {}
     all_scores = {}
     for metric in metrics:

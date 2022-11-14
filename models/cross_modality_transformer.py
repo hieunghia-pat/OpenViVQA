0833259e50b056754c5004f13a4055965934e54f
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-11-14 17:00:21 +0700

    updated some encoder modules

--- models/cross_modality_transformer.py
+++ models/cross_modality_transformer.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from .base_transformer import BaseTransformer
+from .base_classification import BaseClassificationModel
 from utils.instance import InstanceList
 from builders.encoder_builder import build_encoder
 from builders.text_embedding_builder import build_text_embedding
@@ -25,7 +25,7 @@ class MLP(nn.Module):
         return output
 
 @META_ARCHITECTURE.register()
-class CrossModalityTransformer(BaseTransformer):
+class CrossModalityTransformer(BaseClassificationModel):
     '''
         This model is designed follow the idea of LXMERT (https://arxiv.org/pdf/1908.07490.pdf).
     '''
@@ -35,8 +35,6 @@ class CrossModalityTransformer(BaseTransformer):
         self.device = torch.device(config.DEVICE)
 
         self.region_embedding = build_vision_embedding(config.REGION_EMBEDDING)
-        self.grid_embedding = build_vision_embedding(config.GRID_EMBEDDING)
-        self.box_embedding = build_vision_embedding(config.BOX_EMBEDDING)
         self.text_embedding = build_text_embedding(config.TEXT_EMBEDDING, vocab)
 
         self.encoder = build_encoder(config.ENCODER)
@@ -51,32 +49,8 @@ class CrossModalityTransformer(BaseTransformer):
         self.classify = nn.Linear(config.D_MODEL, vocab.total_answers)
         
     def forward(self, input_features: InstanceList):
-        region_features = input_features.region_features
-        region_features, region_padding_mask = self.region_embedding(region_features)
-        region_feat_tokens = torch.ones((region_features.shape[0], region_features.shape[1])).long().to(region_features.device) * self.vocab.feat_idx
-        region_feat_embedded, _ = self.decoder.word_emb(region_feat_tokens)
-        region_features += region_feat_embedded
-
-        region_boxes = input_features.region_boxes
-        region_boxes, region_boxes_padding_mask = self.box_embedding(region_boxes)
-        region_box_tokens = torch.ones((region_boxes.shape[0], region_boxes.shape[1])).long().to(region_boxes.device) * self.vocab.box_idx
-        region_box_embedded, _ = self.decoder.word_emb(region_box_tokens)
-        region_boxes += region_box_embedded
-
-        grid_features = input_features.grid_features
-        grid_features, grid_padding_mask = self.grid_embedding(grid_features)
-        grid_feat_tokens = torch.ones((grid_features.shape[0], grid_features.shape[1])).long().to(grid_features.device) * self.vocab.feat_idx
-        grid_feat_embedded, _ = self.decoder.word_emb(grid_feat_tokens)
-        grid_features += grid_feat_embedded
-        
-        grid_boxes = input_features.grid_boxes
-        grid_boxes, grid_boxes_padding_mask = self.box_embedding(grid_boxes)
-        grid_box_tokens = torch.ones((grid_boxes.shape[0], grid_boxes.shape[1])).long().to(grid_boxes.device) * self.vocab.box_idx
-        grid_box_embedded, _ = self.decoder.word_emb(grid_box_tokens)
-        grid_boxes += grid_box_embedded
-        
-        vision_features = torch.cat([region_features, region_boxes, grid_features, grid_boxes], dim=1)
-        vision_padding_mask = torch.cat([region_padding_mask, region_boxes_padding_mask, grid_padding_mask, grid_boxes_padding_mask], dim=-1)
+        vision_features = input_features.region_features
+        vision_features, vision_padding_mask = self.region_embedding(vision_features)
 
         question_tokens = input_features.question_tokens
         text_features, (text_padding_mask, _) = self.text_embedding(question_tokens)
@@ -85,7 +59,6 @@ class CrossModalityTransformer(BaseTransformer):
         vision_features, text_features = self.encoder(
             vision_features=vision_features,
             vision_padding_mask=vision_padding_mask,
-            boxes=input_features.boxes,
             language_features=text_features,
             language_padding_mask=text_padding_mask
         )

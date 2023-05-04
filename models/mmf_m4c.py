a482c8550d1e3f5d0acaa2cd5857ec580fa95196
hieunghia.nhn <19520178@gm.uit.edu.vn>
2023-05-04 11:02:15 +0700

    updated mmf_m4c and pretrained language models from dev branch

--- models/mmf_m4c.py
+++ models/mmf_m4c.py
@@ -1,4 +1,3 @@
-import functools
 import math
 
 import torch
@@ -7,13 +6,14 @@ import torch.nn.functional as F
 from torch import nn
 from transformers.models.bert.modeling_bert import (
     BertConfig,
-    BertEmbeddings,
     BertEncoder,
     BertPreTrainedModel,
 )
 
 from utils.logging_utils import setup_logger
 from builders.model_builder import META_ARCHITECTURE
+from builders.text_embedding_builder import build_text_embedding
+from models.utils import generate_padding_mask, generate_sequential_mask
 
 logger = setup_logger()
 
@@ -44,31 +44,7 @@ class MMF_M4C(nn.Module):
         self._build_output()
 
     def _build_txt_encoding(self):
-        TEXT_BERT_HIDDEN_SIZE = 768
-
-        self.text_bert_config = BertConfig(hidden_size=self.config.TEXT_BERT.HIDDEN_SIZE,
-                                            num_hidden_layers=self.config.TEXT_BERT.NUM_HIDDEN_LAYERS,
-                                            num_attention_heads=self.config.MMT.NUM_ATTENTION_HEADS)
-        if self.config.TEXT_BERT.LOAD_PRETRAINED:
-            self.text_bert = TextBert.from_pretrained(
-                self.config.TEXT_BERT.PRETRAINED_NAME, config=self.text_bert_config
-            )
-        else:
-            self.text_bert = TextBert(self.text_bert_config)
-
-        # if the text bert output dimension doesn't match the
-        # multimodal transformer (mmt) hidden dimension,
-        # add a linear projection layer between the two
-        if self.mmt_config.hidden_size != TEXT_BERT_HIDDEN_SIZE:
-            logger.info(
-                f"Projecting text_bert output to {self.mmt_config.hidden_size} dim"
-            )
-
-            self.text_bert_out_linear = nn.Linear(
-                self.config.TEXT_BERT.HIDDEN_SIZE, self.mmt_config.hidden_size
-            )
-        else:
-            self.text_bert_out_linear = nn.Identity()
+        self.text_bert = build_text_embedding(self.config.TEXT_BERT, self.vocab)
 
     def _build_obj_encoding(self):
         self.linear_obj_feat_to_mmt_in = nn.Linear(
@@ -126,12 +102,12 @@ class MMF_M4C(nn.Module):
         return results
 
     def _forward_txt_encoding(self, items, fwd_results):
-        fwd_results["txt_inds"] = items.question_tokens
+        fwd_results["txt_inputs"] = items.question
 
         # binary mask of valid text (question words) vs padding
-        text_len = (items.question_tokens != self.vocab.padding_idx).sum(dim=-1)
-        fwd_results["txt_mask"] = _get_mask(
-            text_len, items.question_tokens.size(1)
+        fwd_results["txt_mask"] = generate_padding_mask(
+            items.question_tokens,
+            padding_idx=self.vocab.padding_idx
         )
 
     def _forward_obj_encoding(self, items, fwd_results):
@@ -145,8 +121,10 @@ class MMF_M4C(nn.Module):
         fwd_results["obj_mmt_in"] = obj_mmt_in
 
         # binary mask of valid object vs padding
-        obj_nums = (items.region_features.sum(dim=-1) != 0).sum(dim=-1)
-        fwd_results["obj_mask"] = _get_mask(obj_nums, obj_mmt_in.size(1))
+        fwd_results["obj_mask"] = generate_padding_mask(
+            obj_feat,
+            padding_idx=0
+        )
 
     def _forward_ocr_encoding(self, items, fwd_results):
         # OCR FastText feature (300-dim)
@@ -174,15 +152,16 @@ class MMF_M4C(nn.Module):
         fwd_results["ocr_mmt_in"] = ocr_mmt_in
 
         # binary mask of valid OCR vs padding
-        ocr_nums = (items.ocr_det_features.sum(dim=-1) != 0).sum(dim=-1)
-        fwd_results["ocr_mask"] = _get_mask(ocr_nums, ocr_mmt_in.size(1))
+        fwd_results["ocr_mask"] = generate_padding_mask(
+            ocr_feat,
+            padding_idx=0
+        )
 
     def _forward_mmt(self, items, fwd_results):
         # first forward the text BERT layers
-        text_bert_out = self.text_bert(
-            txt_inds=fwd_results["txt_inds"], txt_mask=fwd_results["txt_mask"]
+        fwd_results["txt_emb"], fwd_results["txt_mask"] = self.text_bert(
+            fwd_results["txt_inputs"]
         )
-        fwd_results["txt_emb"] = self.text_bert_out_linear(text_bert_out)
 
         mmt_results = self.mmt(
             txt_emb=fwd_results["txt_emb"],
@@ -232,30 +211,6 @@ class MMF_M4C(nn.Module):
                 last_ids = torch.where(last_ids == self.vocab.eos_idx, last_ids, argmax_inds[:, ith])
                 if last_ids.mean() == self.vocab.eos_idx:
                     break
-
-class TextBert(BertPreTrainedModel):
-    def __init__(self, config):
-        super().__init__(config)
-
-        self.embeddings = BertEmbeddings(config)
-        self.encoder = BertEncoder(config)
-        self.init_weights()
-
-    def forward(self, txt_inds, txt_mask):
-        encoder_inputs = self.embeddings(txt_inds)
-        attention_mask = txt_mask
-
-        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
-        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
-        assert not extended_attention_mask.requires_grad
-        head_mask = [None] * self.config.num_hidden_layers
-
-        encoder_outputs = self.encoder(
-            encoder_inputs, extended_attention_mask, head_mask=head_mask
-        )
-        seq_output = encoder_outputs[0]
-
-        return seq_output
 
 
 class MMT(BertPreTrainedModel):
@@ -316,12 +271,8 @@ class MMT(BertPreTrainedModel):
             1, 1, from_seq_length, 1
         )
         # decoding step elements can attend to themselves in a causal manner
-        extended_attention_mask[:, :, -dec_max_num:, -dec_max_num:] = _get_causal_mask(
-            dec_max_num, encoder_inputs.device
-        )
+        extended_attention_mask[:, :, -dec_max_num:, -dec_max_num:] = generate_sequential_mask(dec_max_num)
 
-        # flip the mask, so that invalid attention pairs have -10000.
-        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
         assert not extended_attention_mask.requires_grad
         head_mask = [None] * self.config.num_hidden_layers
 
@@ -356,10 +307,6 @@ class OcrPtrNet(nn.Module):
         self.key = nn.Linear(hidden_size, query_key_size)
 
     def forward(self, query_inputs, key_inputs, attention_mask):
-        extended_attention_mask = (1.0 - attention_mask) * -10000.0
-        assert extended_attention_mask.dim() == 2
-        extended_attention_mask = extended_attention_mask.unsqueeze(1)
-
         query_layer = self.query(query_inputs)
         if query_layer.dim() == 2:
             query_layer = query_layer.unsqueeze(1)
@@ -370,7 +317,7 @@ class OcrPtrNet(nn.Module):
 
         scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
         scores = scores / math.sqrt(self.query_key_size)
-        scores = scores + extended_attention_mask
+        scores = scores + attention_mask
         if squeeze_result:
             scores = scores.squeeze(1)
 
@@ -424,25 +371,6 @@ class PrevPredEmbeddings(nn.Module):
         dec_emb = raw_dec_emb + embeddings
 
         return dec_emb
-
-def _get_mask(nums, max_num):
-    # non_pad_mask: b x lq, torch.float32, 0. on PAD
-    batch_size = nums.size(0)
-    arange = torch.arange(0, max_num).unsqueeze(0).expand(batch_size, -1)
-    non_pad_mask = arange.to(nums.device).lt(nums.unsqueeze(-1))
-    non_pad_mask = non_pad_mask.type(torch.float32)
-    return non_pad_mask
-
-
-@functools.lru_cache(maxsize=32)
-def _get_causal_mask(seq_length, device):
-    # generate a lower triangular mask
-    mask = torch.zeros(seq_length, seq_length, device=device)
-    for i in range(seq_length):
-        for j in range(i + 1):
-            mask[i, j] = 1.0
-    return mask
-
 
 def _batch_gather(x, inds):
     assert x.dim() == 3
--- models/modules/text_embeddings.py
+++ models/modules/text_embeddings.py
@@ -6,7 +6,48 @@ from builders.text_embedding_builder import META_TEXT_EMBEDDING
 from builders.word_embedding_builder import build_word_embedding
 from models.utils import generate_sequential_mask, generate_padding_mask
 
-from transformers import BertTokenizer, BertModel, AlbertTokenizer, AlbertModel, T5Tokenizer, T5EncoderModel
+from transformers.models.bert.modeling_bert import (
+    BertConfig,
+    BertEmbeddings,
+    BertEncoder,
+    BertPreTrainedModel,
+)
+
+from transformers.models.albert.modeling_albert import (
+    AlbertConfig,
+    AlbertEmbeddings,
+    AlbertTransformer,
+    AlbertPreTrainedModel
+)
+
+from transformers.models.roberta.modeling_roberta import (
+    RobertaConfig,
+    RobertaEmbeddings,
+    RobertaEncoder,
+    RobertaPreTrainedModel
+)
+
+from transformers.models.deberta_v2.modeling_deberta_v2 import (
+    DebertaV2Config,
+    DebertaV2Embeddings,
+    DebertaV2Encoder,
+    DebertaV2PreTrainedModel
+)
+
+from transformers.models.xlm_roberta.modeling_xlm_roberta import (
+    XLMRobertaConfig,
+    XLMRobertaEmbeddings,
+    XLMRobertaEncoder,
+    XLMRobertaPreTrainedModel
+)
+
+from transformers import (
+    BertTokenizer,
+    AlbertTokenizer, 
+    RobertaTokenizer,
+    DebertaTokenizer,
+    XLMTokenizer
+)
 
 from typing import Dict, List
 import itertools
@@ -239,6 +280,26 @@ class HierarchicalFeaturesExtractor(nn.Module):
 
         return unigram_features, (padding_masks, sequential_masks)
 
+class TextBert(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.embeddings = BertEmbeddings(config)
+        self.encoder = BertEncoder(config)
+        self.init_weights()
+
+    def forward(self, txt_inds, txt_mask):
+        encoder_inputs = self.embeddings(txt_inds)
+        
+        attention_mask = txt_mask
+        head_mask = [None] * self.config.num_hidden_layers
+        encoder_outputs = self.encoder(
+            encoder_inputs, attention_mask, head_mask=head_mask
+        )
+        seq_output = encoder_outputs[0]
+
+        return seq_output
+    
 @META_TEXT_EMBEDDING.register()
 class BertEmbedding(nn.Module):
     def __init__(self, config, vocab):
@@ -246,26 +307,58 @@ class BertEmbedding(nn.Module):
 
         self.device = config.DEVICE
 
-        self.tokenizer = BertTokenizer.from_pretrained(config.PRETRAINED_NAME)
-        self.embedding = BertModel.from_pretrained(config.PRETRAINED_NAME)
-        # freeze all parameters of pretrained model
-        for param in self.embedding.parameters():
-            param.requires_grad = False
+        bert_config = BertConfig(
+            hidden_size=config.HIDDEN_SIZE,
+            num_hidden_layers=config.NUM_HIDDEN_LAYERS,
+            num_attention_heads=config.NUM_ATTENTION_HEADS
+        )
 
-        self.proj = nn.Linear(config.D_PRETRAINED_FEATURE, config.D_MODEL)
+        self.tokenizer = BertTokenizer.from_pretrained(config.PRETRAINED_NAME)
+        self.embedding = TextBert(bert_config)
+        if config.LOAD_PRETRAINED:
+            self.embedding = self.embedding.from_pretrained(config.PRETRAINED_NAME)
+            # freeze all parameters of pretrained model
+            for param in self.embedding.parameters():
+                param.requires_grad = False
+
+        self.proj = nn.Linear(config.HIDDEN_SIZE, config.D_MODEL)
         self.gelu = nn.GELU()
         self.dropout = nn.Dropout(config.DROPOUT)
 
     def forward(self, questions: List[str]):
-        inputs = self.tokenizer(questions, return_tensors="pt", padding=True).to(self.device)
-        padding_mask = generate_padding_mask(inputs.input_ids, padding_idx=self.tokenizer.pad_token_id)
-        features = self.embedding(**inputs).last_hidden_state
+        inputs = self.tokenizer(questions, return_tensors="pt", padding=True).input_ids.to(self.device)
+        padding_mask = generate_padding_mask(inputs, padding_idx=self.tokenizer.pad_token_id)
+        features = self.embedding(inputs, padding_mask)
 
         out = self.proj(features)
         out = self.dropout(self.gelu(out))
 
         return out, padding_mask
+    
+class TextAlbert(AlbertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
 
+        self.config = config
+
+        self.embeddings = AlbertEmbeddings(config)
+        self.embedding_hidden_mapping_in = nn.Linear(config.embedding_size, config.hidden_size)
+        self.encoder = AlbertTransformer(config)
+        self.init_weights()
+
+    def forward(self, txt_inds, txt_mask):
+        embedded_inputs = self.embeddings(txt_inds)
+        encoder_inputs = self.embedding_hidden_mapping_in(embedded_inputs)
+        
+        attention_mask = txt_mask
+        head_mask = [None] * self.config.num_hidden_layers
+        encoder_outputs = self.encoder(
+            encoder_inputs, attention_mask, head_mask=head_mask
+        )
+        seq_output = encoder_outputs[0]
+
+        return seq_output
+    
 @META_TEXT_EMBEDDING.register()
 class AlbertEmbedding(nn.Module):
     def __init__(self, config, vocab):
@@ -273,40 +366,201 @@ class AlbertEmbedding(nn.Module):
 
         self.device = config.DEVICE
 
-        self.tokenizer = AlbertTokenizer.from_pretrained(config.PRETRAINED_NAME)
-        self.embedding = AlbertModel.from_pretrained(config.PRETRAINED_NAME)
-        # freeze all parameters of pretrained model
-        for param in self.embedding.parameters():
-            param.requires_grad = False
+        albert_config = AlbertConfig(
+            hidden_size=config.HIDDEN_SIZE,
+            num_hidden_layers=config.NUM_HIDDEN_LAYERS,
+            num_attention_heads=config.NUM_ATTENTION_HEADS
+        )
 
-        self.proj = nn.Linear(config.D_PRETRAINED_FEATURE, config.D_MODEL)
+        self.tokenizer = AlbertTokenizer.from_pretrained(config.PRETRAINED_NAME)
+        self.embedding = TextAlbert(albert_config)
+        if config.LOAD_PRETRAINED:
+            self.embedding = self.embedding.from_pretrained(config.PRETRAINED_NAME)
+            # freeze all parameters of pretrained model
+            for param in self.embedding.parameters():
+                param.requires_grad = False
+
+        self.proj = nn.Linear(config.HIDDEN_SIZE, config.D_MODEL)
         self.gelu = nn.GELU()
         self.dropout = nn.Dropout(config.DROPOUT)
 
     def forward(self, questions: List[str]):
-        inputs = self.tokenizer(questions, return_tensors="pt", padding=True).to(self.device)
-        padding_mask = generate_padding_mask(inputs.input_ids, padding_idx=self.tokenizer.pad_token_id)
-        features = self.embedding(**inputs).last_hidden_state
+        inputs = self.tokenizer(questions, return_tensors="pt", padding=True).input_ids.to(self.device)
+        padding_mask = generate_padding_mask(inputs, padding_idx=self.tokenizer.pad_token_id)
+        features = self.embedding(inputs, padding_mask)
 
         out = self.proj(features)
         out = self.dropout(self.gelu(out))
 
         return out, padding_mask
 
+class TextRoberta(RobertaPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.config = config
+
+        self.embeddings = RobertaEmbeddings(config)
+        self.encoder = RobertaEncoder(config)
+        self.init_weights()
+
+    def forward(self, txt_inds, txt_mask):
+        encoder_inputs = self.embeddings(txt_inds)
+        
+        attention_mask = txt_mask
+        head_mask = [None] * self.config.num_hidden_layers
+        encoder_outputs = self.encoder(
+            encoder_inputs, attention_mask, head_mask=head_mask
+        )
+        seq_output = encoder_outputs[0]
+
+        return seq_output
+    
 @META_TEXT_EMBEDDING.register()
-class T5Embedding(nn.Module):
+class RobertaEmbedding(nn.Module):
     def __init__(self, config, vocab):
         super().__init__()
 
         self.device = config.DEVICE
 
-        self.tokenizer = T5Tokenizer.from_pretrained(config.PRETRAINED_NAME)
-        self.embedding = T5EncoderModel.from_pretrained(config.PRETRAINED_NAME)
+        roberta_config = RobertaConfig(
+            hidden_size=config.HIDDEN_SIZE,
+            num_hidden_layers=config.NUM_HIDDEN_LAYERS,
+            num_attention_heads=config.NUM_ATTENTION_HEADS
+        )
+
+        self.tokenizer = RobertaTokenizer.from_pretrained(config.PRETRAINED_NAME)
+        self.embedding = TextRoberta(roberta_config)
+        if config.LOAD_PRETRAINED:
+            self.embedding = self.embedding.from_pretrained(config.PRETRAINED_NAME)
+            # freeze all parameters of pretrained model
+            for param in self.embedding.parameters():
+                param.requires_grad = False
+
+        self.proj = nn.Linear(config.HIDDEN_SIZE, config.D_MODEL)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(config.DROPOUT)
 
     def forward(self, questions: List[str]):
-        input_ids = self.tokenizer(questions, return_tensors='pt', padding=True).input_ids.to(self.device)
-        padding_mask = generate_padding_mask(input_ids, padding_idx=self.tokenizer.pad_token_id)
+        inputs = self.tokenizer(questions, return_tensors="pt", padding=True).input_ids.to(self.device)
+        padding_mask = generate_padding_mask(inputs, padding_idx=self.tokenizer.pad_token_id)
+        features = self.embedding(inputs, padding_mask)
 
-        out = self.embedding(input_ids=input_ids, decoder_input_ids=input_ids).last_hidden_states
+        out = self.proj(features)
+        out = self.dropout(self.gelu(out))
+
+        return out, padding_mask
+    
+class TextDeberta_v2(DebertaV2PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.config = config
+
+        self.embeddings = DebertaV2Embeddings(config)
+        self.encoder = DebertaV2Encoder(config)
+        self.init_weights()
+
+    def forward(self, txt_inds, txt_mask):
+        encoder_inputs = self.embeddings(txt_inds)
+        
+        attention_mask = txt_mask
+        head_mask = [None] * self.config.num_hidden_layers
+        encoder_outputs = self.encoder(
+            encoder_inputs, attention_mask, head_mask=head_mask
+        )
+        seq_output = encoder_outputs[0]
+
+        return seq_output
+    
+@META_TEXT_EMBEDDING.register()
+class DebertaEmbedding(nn.Module):
+    def __init__(self, config, vocab):
+        super().__init__()
+
+        self.device = config.DEVICE
+
+        deberta_config = DebertaV2Config(
+            hidden_size=config.HIDDEN_SIZE,
+            num_hidden_layers=config.NUM_HIDDEN_LAYERS,
+            num_attention_heads=config.NUM_ATTENTION_HEADS
+        )
+
+        self.tokenizer = DebertaTokenizer.from_pretrained(config.PRETRAINED_NAME)
+        self.embedding = TextDeberta_v2(deberta_config)
+        if config.LOAD_PRETRAINED:
+            self.embedding = self.embedding.from_pretrained(config.PRETRAINED_NAME)
+            # freeze all parameters of pretrained model
+            for param in self.embedding.parameters():
+                param.requires_grad = False
+
+        self.proj = nn.Linear(config.HIDDEN_SIZE, config.D_MODEL)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(config.DROPOUT)
+
+    def forward(self, questions: List[str]):
+        inputs = self.tokenizer(questions, return_tensors="pt", padding=True).input_ids.to(self.device)
+        padding_mask = generate_padding_mask(inputs, padding_idx=self.tokenizer.pad_token_id)
+        features = self.embedding(inputs, padding_mask)
+
+        out = self.proj(features)
+        out = self.dropout(self.gelu(out))
+
+        return out, padding_mask
+    
+class TextXLM(XLMRobertaPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.config = config
+
+        self.embeddings = XLMRobertaEmbeddings(config)
+        self.encoder = XLMRobertaEncoder(config)
+        self.init_weights()
+
+    def forward(self, txt_inds, txt_mask):
+        encoder_inputs = self.embeddings(txt_inds)
+        
+        attention_mask = txt_mask
+        head_mask = [None] * self.config.num_hidden_layers
+        encoder_outputs = self.encoder(
+            encoder_inputs, attention_mask, head_mask=head_mask
+        )
+        seq_output = encoder_outputs[0]
+
+        return seq_output
+    
+@META_TEXT_EMBEDDING.register()
+class XLMRobertaEmbedding(nn.Module):
+    def __init__(self, config, vocab):
+        super().__init__()
+
+        self.device = config.DEVICE
+
+        xlm_config = XLMRobertaConfig(
+            hidden_size=config.HIDDEN_SIZE,
+            num_hidden_layers=config.NUM_HIDDEN_LAYERS,
+            num_attention_heads=config.NUM_ATTENTION_HEADS
+        )
+
+        self.tokenizer = XLMTokenizer.from_pretrained(config.PRETRAINED_NAME)
+        self.embedding = TextXLM(xlm_config)
+        if config.LOAD_PRETRAINED:
+            self.embedding = self.embedding.from_pretrained(config.PRETRAINED_NAME)
+            # freeze all parameters of pretrained model
+            for param in self.embedding.parameters():
+                param.requires_grad = False
+
+        self.proj = nn.Linear(config.HIDDEN_SIZE, config.D_MODEL)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(config.DROPOUT)
+
+    def forward(self, questions: List[str]):
+        inputs = self.tokenizer(questions, return_tensors="pt", padding=True).input_ids.to(self.device)
+        padding_mask = generate_padding_mask(inputs, padding_idx=self.tokenizer.pad_token_id)
+        features = self.embedding(inputs, padding_mask)
+
+        out = self.proj(features)
+        out = self.dropout(self.gelu(out))
 
         return out, padding_mask

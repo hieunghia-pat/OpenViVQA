1e0b6fa1d0be900adf2462d36d50e378f9ad6a92
hieunghia-pat <19520178@gm.uit.edu.vn>
2023-05-17 14:18:16 +0700

    masked out undebugged models

--- models/__init__.py
+++ models/__init__.py
@@ -10,13 +10,13 @@ from .iterative_saaa import IterativeSAAA
 from .joint_transformer import JointTransformer
 from .unique_transformer import UniqueTransformer
 from .m4c import M4C
-from .mmf_iterative_m4c import MMF_IterativeM4C
-from .mmf_iterative_multilevel_m4c import MMF_Iterative_Multilevel_M4C
+# from .mmf_iterative_m4c import MMF_IterativeM4C
+# from .mmf_iterative_multilevel_m4c import MMF_Iterative_Multilevel_M4C
 from .mmf_m4c import MMF_M4C
-from .mmf_language_adaptive_m4c import MMF_LanguageAdaptiveM4C
-from .mmf_regional_m4c import MMF_REGIONAL_M4C
+# from .mmf_language_adaptive_m4c import MMF_LanguageAdaptiveM4C
+# from .mmf_regional_m4c import MMF_REGIONAL_M4C
 from .mmf_lorra import MMF_LoRRA
-from .mmf_iterative_lorra import MMF_IterativeLoRRA
+# from .mmf_iterative_lorra import MMF_IterativeLoRRA
 from .visiolinguistic_transformer import VisiolinguisticTransformer
 from .vit_mt5 import ViTmT5
 from .vit_mbert_classification import ViTmBERTClassification

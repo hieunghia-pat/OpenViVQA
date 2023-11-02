c474d9796f1510373c61aae58dbc9d950515249c
hieunghia-pat <19520178@gm.uit.edu.vn>
2023-11-02 14:42:22 +0700

    removed QA type in dictionary dataset

--- data_utils/datasets/dictionary_dataset.py
+++ data_utils/datasets/dictionary_dataset.py
@@ -21,7 +21,6 @@ class DictionaryDataset(BaseDataset):
                     answers = [" ".join(answer) for answer in answers]
                     annotation = {
                         "question_id": ann["id"],
-                        "type": ann["QA-type"],
                         "question": question,
                         "answers": answers,
                         "image_id": ann["image_id"],
@@ -44,7 +43,6 @@ class DictionaryDataset(BaseDataset):
 
         return Instance(
             question_id=item["question_id"],
-            type=item["type"],
             image_id=image_id,
             filename=filename,
             question=question,

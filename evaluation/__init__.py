2d2d99818d45ef4ae252d7ea650a7e48e4b25010
minhquan6203 <harryquan.minh@gmail.com>
2023-11-05 08:50:57 +0700

    update

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -6,13 +6,21 @@ from .accuracy import Accuracy
 from .f1 import F1
 from .precision import Precision
 from .recall import Recall
+import string
+
+def normalize_text(text):
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    text = text.lower().strip()
+    return text
 
 def compute_scores(gts, gen):
     metrics = (Bleu(), Meteor(), Rouge(), Cider(), Accuracy(), Precision(), Recall(), F1())
     all_score = {}
     all_scores = {}
+    normalized_gts = {key: [normalize_text(value[0])] for key, value in gts.items()}
+    normalized_gen = {key: [normalize_text(value[0])] for key, value in gen.items()}
     for metric in metrics:
-        score, scores = metric.compute_score(gts, gen)
+        score, scores = metric.compute_score(normalized_gts, normalized_gen)
         all_score[str(metric)] = score
         all_scores[str(metric)] = scores
 

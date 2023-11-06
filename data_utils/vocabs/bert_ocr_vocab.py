84612a6952be0c13c2422e83d969d4b37de2d709
hieunghia-pat <19520178@gm.uit.edu.vn>
2023-11-06 16:22:11 +0700

    refined the answer decoding of bert ocr vocab

--- data_utils/vocabs/bert_ocr_vocab.py
+++ data_utils/vocabs/bert_ocr_vocab.py
@@ -1,3 +1,5 @@
+import torch
+
 from transformers import BertTokenizer
 
 from builders.vocab_builder import META_VOCAB
@@ -7,6 +9,7 @@ from data_utils.utils import preprocess_sentence
 from typing import List, Dict
 import numpy as np
 import json
+import re
 
 @META_VOCAB.register()
 class BertOcrVocab(OcrVocab):
@@ -65,8 +68,69 @@ class BertOcrVocab(OcrVocab):
             for ann in json_data["annotations"]:
                 for answer in ann["answers"]:
                     question = preprocess_sentence(ann["question"], self.tokenizer)
-                    answer = preprocess_sentence(answer, self.tokenizer)
+                    answer = preprocess_sentence(answer, self.tokenizer, self.tokenizer)
                     if len(question) + 2 > self.max_question_length:
                             self.max_question_length = len(question) + 2
                     if len(answer) + 2 > self.max_answer_length:
                         self.max_answer_length = len(answer) + 2
+
+    def decode_answer(self, answer_vecs: torch.Tensor, list_ocr_tokens: List[List[str]], join_words=True) -> List[str]:
+        '''
+            answer_vecs: (bs, max_length)
+        '''
+        ocr_token_of = [{len(self.stoi)+idx: token for idx, token in enumerate(ocr_tokens)} for ocr_tokens in list_ocr_tokens]
+        answers = []
+        for batch, vec in enumerate(answer_vecs):
+            answer = []
+            for idx in vec.tolist():
+                if idx in ocr_token_of[batch]:
+                    word = ocr_token_of[batch][idx]
+                else:
+                    word = self.itos[idx]
+                if word == self.eos_token:
+                    break
+                if word not in self.specials:
+                    answer.append(word)
+            answer = " ".join(answer)
+            answer = re.sub(r"#+", "", answer)
+
+            if join_words:
+                answers.append(answer)
+            else:
+                answers.append(answer.strip().split())
+
+        return answers
+
+    def decode_answer_with_determination(self, answer_vecs: torch.Tensor, list_ocr_tokens: List[List[str]], join_words=True) -> List[str]:
+        '''
+            This module is designed to determine if a selected token is in fixed vocab or from ocr token
+            params:
+                - answer_vecs: (bs, max_length)
+        '''
+        ocr_token_of = [{len(self.stoi)+idx: token for idx, token in enumerate(ocr_tokens)} for ocr_tokens in list_ocr_tokens]
+        answers = []
+        in_fixed_vocab = []
+        for batch, vec in enumerate(answer_vecs):
+            answer = []
+            in_fixed_vocab_per_batch = []
+            for idx in vec.tolist():
+                if idx in ocr_token_of[batch]:
+                    word = ocr_token_of[batch][idx]
+                    in_fixed_vocab_per_batch.append(False)
+                else:
+                    word = self.itos[idx]
+                    in_fixed_vocab_per_batch.append(True)
+                if word == self.eos_token:
+                    break
+                if word not in self.specials:
+                    answer.append(word)
+            answer = " ".join(answer)
+            answer = re.sub(r"#+", "", answer)
+
+            if join_words:
+                answers.append(answer)
+            else:
+                answers.append(answer.strip().split())
+            in_fixed_vocab.append(in_fixed_vocab_per_batch)
+
+        return answers, in_fixed_vocab

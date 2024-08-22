aa139621387b8f455dafd12dfe3e55352588440f
Nguyen Ngoc Yen Nhi <118429842+NhiNguyen34@users.noreply.github.com>
2024-08-22 14:38:38 +0700

    Update training_mmf_task.py

--- tasks/training_mmf_task.py
+++ tasks/training_mmf_task.py
@@ -134,7 +134,7 @@ class TrainingMMF(OpenEndedTask):
 
         while True:
             self.train()
-            # self.evaluate_loss(self.dev_dataloader)
+            self.evaluate_loss(self.dev_dataloader)
 
             # val scores
             scores = self.evaluate_metrics(self.dev_dict_dataloader)
